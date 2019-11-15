413276bff29435684c431310cb510c44102bde33
Matheus Henrick <contatomatheushenrick@gmail.com>
2019-11-15 17:12:42 -0300

    v1.1.1 - Bug fixes

--- src/GUI/Menu.java
+++ src/GUI/Menu.java
@@ -6,10 +6,6 @@
 package GUI;
 
 import java.awt.Toolkit;
-import java.util.ArrayList;
-import java.util.List;
-import model.Cliente;
-import model.Veiculo;
 
 /**
  *
@@ -48,7 +44,6 @@ public class Menu extends javax.swing.JFrame {
 
         setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
         setCursor(new java.awt.Cursor(java.awt.Cursor.DEFAULT_CURSOR));
-        setIconImage(new javax.swing.ImageIcon(getClass().getResourse("programicon2.png")););
         setResizable(false);
 
         jLabel1.setFont(new java.awt.Font("Century", 0, 36)); // NOI18N
