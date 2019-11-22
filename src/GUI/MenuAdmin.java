240c10f0676a5b7349edf4b024040a0fb3d40d85
Matheus Henrick <contatomatheushenrick@gmail.com>
2019-11-22 16:49:23 -0300

    Atualizando v4.0

--- src/GUI/MenuAdmin.java
+++ src/GUI/MenuAdmin.java
@@ -28,7 +28,51 @@ public class MenuAdmin extends javax.swing.JFrame {
         setLocationRelativeTo(null);
         setIcon();
         
+         //Criando objetos para teste
+        Cliente c = new Cliente();
         
+        c.setNome("Matheus Henrick");
+        c.setSexo("Masculino");
+        c.setDataNascimento("08/02/1999");
+        c.setCpf("043.545.571-00");
+        c.setId(1);
+        c.setTelefone("(62) 9 9299-9449 ");
+        c.setEnd("Avenida Maria Pestana, QD 77 LT 7 - Jardim Balneário M. Ponte");
+        
+        Marca marca = new Marca();
+        
+        marca.setNome("Volkswagem");
+        
+        Modelo modelo = new Modelo();
+        
+        modelo.setMarca(marca);
+        modelo.setNomeModelo("Gol");
+        modelo.setAno(2008);
+        
+        Veiculo v = new Veiculo();
+        
+        v.setPlaca("NLA-1194");
+        v.setCor("Vermelho");
+        v.setValor(12500);
+        v.setModelo(modelo);
+        
+        Vendedor vendedor = new Vendedor();
+        
+        vendedor.setMatricula(1);
+        vendedor.setCpf("001.532.173-27");
+        vendedor.setDataNascimento("10/11/1981");
+        vendedor.setNome("Lorena Larissa");
+        vendedor.setSexo("Feminino");
+        vendedor.setEnd("Rua B, Quadra 16 - Monções, Goiânia, GO.");
+        vendedor.setTelefone("(62) 9 9188-2936");
+        
+        //Adicionando objetos ás listas
+        Cadastramento.adicionaCliente(c);
+        Cadastramento.adicionaVeiculo(v);
+        Cadastramento.adicionaVeiculoDisponivel(v);
+        Cadastramento.adicionaMarca(marca);
+        Cadastramento.adicionaModelo(modelo);
+        Cadastramento.adicionaVendedor(vendedor);
         
     }
 
